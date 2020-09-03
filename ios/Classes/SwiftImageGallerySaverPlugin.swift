79daf2cae2a98289bc40d392e830bad39e405fcb
haibin <13688065326@qq.com>
2020-09-03 21:37:41 +0800

    update save image return path

--- ios/Classes/SwiftImageGallerySaverPlugin.swift
+++ ios/Classes/SwiftImageGallerySaverPlugin.swift
@@ -1,8 +1,10 @@
 import Flutter
 import UIKit
+import Photos
 
 public class SwiftImageGallerySaverPlugin: NSObject, FlutterPlugin {
     var result: FlutterResult?;
+    var localId:String = "";
 
     public static func register(with registrar: FlutterPluginRegistrar) {
       let channel = FlutterMethodChannel(name: "image_gallery_saver", binaryMessenger: registrar.messenger())
@@ -20,12 +22,14 @@ public class SwiftImageGallerySaverPlugin: NSObject, FlutterPlugin {
             let name = arguments["name"]
             else { return }
         let newImage = image.jpegData(compressionQuality: CGFloat(quality / 100))!
-        UIImageWriteToSavedPhotosAlbum(UIImage(data: newImage) ?? image , self, #selector(didFinishSavingImage(image:error:contextInfo:)), nil)
+        saveImageInAlbum(image: image)
+//        UIImageWriteToSavedPhotosAlbum(UIImage(data: newImage) ?? image , self, #selector(didFinishSavingImage(image:error:contextInfo:)), nil)
       } else if (call.method == "saveFileToGallery") {
         guard let path = call.arguments as? String else { return }
         if (isImageFile(filename: path)) {
             if let image = UIImage(contentsOfFile: path) {
-                 UIImageWriteToSavedPhotosAlbum(image, self, #selector(didFinishSavingImage(image:error:contextInfo:)), nil)
+                saveImageInAlbum(image: image)
+//                 UIImageWriteToSavedPhotosAlbum(image, self, #selector(didFinishSavingImage(image:error:contextInfo:)), nil)
             }
         } else {
             if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(path)) {
@@ -53,4 +57,76 @@ public class SwiftImageGallerySaverPlugin: NSObject, FlutterPlugin {
             || filename.hasSuffix(".JPG")
             || filename.hasSuffix(".PNG")
     }
+    
+    func isAuthored()->Bool {
+        return PHPhotoLibrary.authorizationStatus() == .authorized || PHPhotoLibrary.authorizationStatus() == .notDetermined
+    }
+    
+    
+    func saveVideoInAlbum(path:String){
+        if !isAuthored() {
+            result?(nil)
+            return
+        }
+        PHPhotoLibrary.shared().performChanges({
+            let result = PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: URL(fileURLWithPath: path))
+            let assetPlaceholder = result?.placeholderForCreatedAsset
+            self.localId = assetPlaceholder?.localIdentifier as! String
+        }){(isSuccess:Bool,error:Error?) in
+            if isSuccess {
+                print("save picture ok;")
+                let assetResult = PHAsset.fetchAssets(withLocalIdentifiers: [self.localId], options: nil)
+                let asset = assetResult[0]
+                let options = PHContentEditingInputRequestOptions()
+                options.canHandleAdjustmentData = {(adjustmeta: PHAdjustmentData)
+                    -> Bool in
+                    return true
+                }
+                //获取保存视频的路径
+                asset.requestContentEditingInput(with: options, completionHandler: {
+                    (contentEditingInput:PHContentEditingInput?, info: [AnyHashable : Any]) in
+                    print("地址：",contentEditingInput!.fullSizeImageURL!)
+                    self.result?(contentEditingInput!.fullSizeImageURL!)
+                })
+                
+            }else{
+                print("save failed;")
+            }
+        }
+    }
+    
+    
+    func saveImageInAlbum(image:UIImage) {
+        if !isAuthored() {
+            result?(nil)
+            return
+        }
+        PHPhotoLibrary.shared().performChanges({
+            let result = PHAssetChangeRequest.creationRequestForAsset(from: image)
+            let assetPlaceholder = result.placeholderForCreatedAsset
+            self.localId = assetPlaceholder?.localIdentifier as! String
+            print(self.localId)
+        }){(isSuccess:Bool,error:Error?) in
+            if isSuccess {
+                print("save picture ok;")
+                let assetResult = PHAsset.fetchAssets(withLocalIdentifiers: [self.localId], options: nil)
+                let asset = assetResult[0]
+                let options = PHContentEditingInputRequestOptions()
+                options.canHandleAdjustmentData = {(adjustmeta: PHAdjustmentData)
+                    -> Bool in
+                    return true
+                }
+                //获取保存的图片路径
+                asset.requestContentEditingInput(with: options, completionHandler: {
+                    (contentEditingInput:PHContentEditingInput?, info: [AnyHashable : Any]) in
+                    print("地址：",contentEditingInput!.fullSizeImageURL!)
+                    self.result?(contentEditingInput!.fullSizeImageURL!.absoluteString)
+                })
+                
+            }else{
+                print("save failed;")
+            }
+        }
+    }
+    
 }
